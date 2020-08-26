229ad72bb51947033bf5d7f50ab7f027b1eb240d
easywayautomaiton-mina <easywayautomation.mina@gmail.com>
2020-08-25 21:24:00 -0400

    NewProject02

---
+++ src/main/java/Demo2/NewProject02.java
@@ -0,0 +1,19 @@
+package Demo2;
+
+public class NewProject02 {
+   public static void main(String[] args) {
+	   
+	   int num = 4;
+	   if (num==2) {
+	      System.out.println(num);
+   } else if (num==3) {
+	   System.out.println(num);
+   }else if (num==4) {
+	   System.out.println(num);
+	      
+   
+   } else {
+	   System.out.println(num);
+   }
+}
+}  
